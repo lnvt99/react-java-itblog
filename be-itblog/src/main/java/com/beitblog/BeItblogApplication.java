576b8eaeaa96996eff0eb0086f18ad90bc588d5a
lnvt99 <lnvthang.it@gmail.com>
2022-08-30 15:33:39 +0700

    Initial project

---
+++ be-itblog/src/main/java/com/beitblog/BeItblogApplication.java
@@ -0,0 +1,13 @@
+package com.beitblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeItblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeItblogApplication.class, args);
+	}
+
+}
---
+++ be-itblog/src/test/java/com/beitblog/BeItblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beitblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeItblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
