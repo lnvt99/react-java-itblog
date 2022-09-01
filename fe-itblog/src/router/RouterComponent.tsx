5f287d0be6ab6a135341a2871ea541c7cde54034
lnvt99 <lnvthang.it@gmail.com>
2022-09-01 14:48:18 +0700

    Comment code

--- fe-itblog/src/router/RouterComponent.tsx
+++ fe-itblog/src/router/RouterComponent.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from "react";
 import {
     Routes,
     Route,
 } from "react-router-dom";
 import CreatePostComponent from "../components/create-post/CreatePostComponent";
 import PostComponent from "../components/home/post/PostComponent";
-
+import { Post } from "../model/PostModel";
+import { dummyDataPost } from "../service/service";
+import data from '../dummy-data/DummyData.json'
 const RouterComponet = () => {
+
+    // const [listPost, setListPost] = useState<Array<Post> | undefined>();
+    const [listPost, setListPost] = useState<[]>();
+    
+
+    // Get API from BE
+    // useEffect(() => {
+    //     async function run() {
+    //         const response = await fetch('');
+    //         const result = await response.json();
+    //         setListPost(result);
+    //     };
+    //     run();
+    // }, [])
+
     return (
         <div>
             <Routes>
-                <Route path="" element={<PostComponent />} />
+                <Route path="/" element={listPost && <PostComponent listPost={listPost}/>} />
                 <Route path="/create" element={<CreatePostComponent />} />
             </Routes>
         </div>
