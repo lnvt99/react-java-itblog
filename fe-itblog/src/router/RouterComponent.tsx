7d9426428205fef550d5524a8e81722b6cc9c85b
lnvt99 <lnvthang.it@gmail.com>
2022-09-08 17:02:05 +0700

    Comment code

--- fe-itblog/src/router/RouterComponent.tsx
+++ fe-itblog/src/router/RouterComponent.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+
 import {
     Routes,
     Route,
-} from "react-router-dom";
-import CreatePostComponent from "../components/create-post/CreatePostComponent";
-import PostComponent from "../components/home/post/PostComponent";
-import { dummyDataPost } from "../service/service";
+} from 'react-router-dom';
+import axios from 'axios';
+
+import PostComponent from '../components/home/post/PostComponent';
+import CreatePostComponent from '../components/create-post/CreatePostComponent';
 import { Post } from '../model/PostModel';
+import { dummyDataPost } from '../service/service';
+
 const RouterComponet = () => {
 
     const [listPost, setListPost] = useState<Array<Post> | undefined>();
@@ -20,6 +24,19 @@ const RouterComponet = () => {
     //     };
     //     run();
     // }, [])
+
+    useEffect(() => {
+        async function getPost() {
+            try {
+                const response = await axios.get('http://localhost:8080/api/categories');
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        getPost()
+    })
 
     return (
         <div>
